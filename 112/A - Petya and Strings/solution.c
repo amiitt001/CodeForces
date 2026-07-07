f632e97840c83a002b9f07a228b302ec20b676f2
Amit Verma <amitverma2k99@gmail.com>
2026-07-07 14:23:43 +0530

    Add Petya and Strings [A] from Codeforces

---
+++ 112/A - Petya and Strings/solution.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main()
+{
+    char lower[100], upper[100];
+    scanf("%s%s", &lower, &upper);
+    for(int i =0; lower[i] != '\0';i++){
+        lower[i] = tolower(lower[i]);
+    }
+    for(int i =0; upper[i] != 0;i++){
+        upper[i]= tolower(upper[i]);
+    }
+    int result = strcmp(lower, upper);
+    if (result > 0)
+    {
+        printf("1
+");
+    }
+    if (result < 0)
+    {
+        printf("-1
+");
+    }
+    if (result == 0)
+    {
+        printf("0
+");
+    }
+    return 0;
+}
